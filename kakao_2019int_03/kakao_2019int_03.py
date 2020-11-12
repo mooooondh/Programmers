70e8e556bd10b2a1c7518be359421c71d189d5c6
mooooondh <mdh9514@gmail.com>
2020-11-12 19:16:10 +0900

    불량 사용자

--- kakao_2019int_03/kakao_2019int_03.py
+++ kakao_2019int_03/kakao_2019int_03.py
@@ -1,50 +1,33 @@
-import itertools
-import re
+from itertools import permutations
+
+def len_check(ban_id, check_id):
+    for i in range(len(ban_id)):
+        if(len(ban_id[i])!= len(check_id[i])):
+            return False
+        if(str_check(ban_id[i], check_id[i])== False):
+            return False
+    return True
+
+def str_check(ban_id, check_id):
+    for i in range(len(ban_id)):
+        if(ban_id[i]== "*"):
+            continue
+        elif(ban_id[i]!= check_id[i]):
+            return False
+    return True
 
 def solution(user_id, banned_id):
-    comp_data= []
-    case_data= [[] for _ in range(len(banned_id))]
+    answer = []
 
-    for i in banned_id:
-        data= list(i)
-        for j, val in enumerate(data):
-            if(val== "*"):
-                data[j]= "."
-        comp_data.append("".join(data))
+    for i in permutations(user_id, len(banned_id)):
+        if(len_check(banned_id, i)== True):
+            i= set(i)
+            if i not in answer:
+                answer.append(i)
 
-    for i, val1 in enumerate(comp_data):
-        p= re.compile(val1)
-        for j, val2 in enumerate(user_id):
-            if(len(val1)== len(val2)):
-                m= p.match(val2)
+    return len(answer)
 
-                if(m!= None):
-                    case_data[i].append(m[0])
 
-    print(case_data)
-    case_list= list(itertools.product(*case_data))
-
-    for i, val in enumerate(case_list):
-        case_list[i]= list(case_list[i])
-        case_list[i].sort()
-
-    i = 0
-
-    while(True):
-        if (len(case_list[i]) != len(list(set(case_list[i])))):
-            case_list.pop(i)
-        else:
-            i+= 1
-
-        if(i== len(case_list)):
-            break
-
-    case_list= list(set(map(tuple, case_list)))
-
-    answer= len(case_list)
-
-    return answer
-
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
-print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
+# print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"]))
+# print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
 print(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"]))
